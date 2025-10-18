45b276835094334093b93e109ea579e2246feaed
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-10-18 23:35:00 +0530

    just a improving code

--- src/streaming/streaming.controller.ts
+++ src/streaming/streaming.controller.ts
@@ -8,10 +8,15 @@ import { createStream, createStreamWithFfmpeg } from 'src/utils/createStream';
 export class StreamingController {
     @Get('/')
     streamMovie(@Res() res: Response, @Req() req: Request) {
+        console.log('got stream request', req.headers);
+        // if (!req.headers['range']) {
+        //     req.headers['range'] = 'bytes=0-';
+        // }
+
         return createStream(res, req);
     }
     @Get('/f')
     streamMovieWithFfmpeg(@Res() res: Response, @Req() req: Request) {
         return createStreamWithFfmpeg(res, req);
-    } 
+    }
 }
